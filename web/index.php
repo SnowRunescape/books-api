860fd94aad354903821faddaf3148ea7c6c97cdb
Bruno Caitano <snowrunescape@snowdev.com.br>
2024-09-15 19:48:39 -0400

    feat: implements debug on env

--- web/index.php
+++ web/index.php
@@ -2,14 +2,15 @@
 
 // comment out the following two lines when deployed to production
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $dotenv = \Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..');
 $dotenv->load();
+
+defined('YII_DEBUG') or define('YII_DEBUG', filter_var(getenv('YII_DEBUG'), FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE));
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
