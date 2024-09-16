b5a7ba05497b237182677803b8538a481e174b78
Bruno Caitano <snowrunescape@snowdev.com.br>
2024-09-16 18:50:08 -0400

    fix: protect route, and fix cors

--- controllers/UploadController.php
+++ controllers/UploadController.php
@@ -11,6 +11,24 @@ class UploadController extends Controller
     const MAX_FILE_SIZE_MB = 2;
     const ACCEPTED_FILE_FORMATS = ['jpg', 'png'];
 
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::class,
+        ];
+
+        unset($behaviors['authenticator']);
+
+        $behaviors['authenticator'] = [
+            'class' => \yii\filters\auth\HttpBearerAuth::class,
+            'except' => ['options'],
+        ];
+
+        return $behaviors;
+    }
+
     public function actionIndex()
     {
         $fileName = (new UploadService())->upload([
